e949e9e2947cd419ce1023754ff4c01bae6d83f2
Lasse <lasse.jenn.may@gmail.com>
2023-03-23 10:45:53 +0100

    new landing page and convert script to embedd leaderboard tabel more nicely

--- convert.sh
+++ convert.sh
@@ -44,32 +44,49 @@ for csv_file in ${csv_dir}/*.csv; do
 
 done
 
-# generate quick and dirty landing page
-html_file="$out_dir/index.html"
-echo "<html>" > "${html_file}"
-echo "<head>" >> "${html_file}"
-echo "<link rel='stylesheet' href='static/style.css'>" >> "${html_file}"
-echo "<style>" >> "${html_file}"
-echo ".outer { display: flex; justify-content: center; align-items: center; height: 100vh; }" >> "${html_file}"
-echo ".heading { text-align: center; }" >> "${html_file}"
-echo ".file-list { margin-left: auto; margin-right: auto; }" >> "${html_file}"
-echo ".file-link { font-weight: bold; }" >> "${html_file}"
-echo "</style>" >> "${html_file}"
-echo "</head>" >> "${html_file}"
-echo "<body>" >> "${html_file}"
-echo "<div class='outer'>" >> "${html_file}"
-echo "<div class='inner'>" >> "${html_file}"
-#echo "<h2 class='heading'>List of Processed Files</h2>" >> "${html_file}"
-echo "<h2 class='heading'>🦾 Real AI Leaderboards 🦿</h2>" >> "${html_file}"
-echo "<table class='file-list'>" >> "${html_file}"
+# generate landing page
+# convert to html
+pandoc -s -o $out_dir/index.html --metadata title="Real AI Gym Leaderboards" --css=static/style.css --template=templates/default.html5 index.md
+
+# in the generated html, search for this string...
+placeholder="<p>:::leaderboard-links:::</p>"
+
+# and replace with this...
+table="<table>"
 for file_name in "${processed_files[@]}"; do
   link_name="${file_name}.html"
-  echo "<tr><td><a href='${link_name}' class='file-link'>${file_name}</a></td></tr>" >> "${html_file}"
+  table+="<tr><td><a href='${link_name}' class='file-link'>${file_name}</a></td></tr>"
 done
-echo "</table>" >> "${html_file}"
-echo "</div>" >> "${html_file}"
-echo "</div>" >> "${html_file}"
-echo "</body>" >> "${html_file}"
-echo "</html>" >> "${html_file}"
-echo "List of processed files written to: ${html_file}"
+table+="</table>"
+
+sed -i "s|${placeholder}|${table}|g" $out_dir/index.html
+
+# generate quick and dirty landing page
+#html_file="$out_dir/index.html"
+#echo "<html>" > "${html_file}"
+#echo "<head>" >> "${html_file}"
+#echo "<link rel='stylesheet' href='static/style.css'>" >> "${html_file}"
+#echo "<style>" >> "${html_file}"
+#echo ".outer { display: flex; justify-content: center; align-items: center; height: 100vh; }" >> "${html_file}"
+#echo ".heading { text-align: center; }" >> "${html_file}"
+#echo ".file-list { margin-left: auto; margin-right: auto; }" >> "${html_file}"
+#echo ".file-link { font-weight: bold; }" >> "${html_file}"
+#echo "</style>" >> "${html_file}"
+#echo "</head>" >> "${html_file}"
+#echo "<body>" >> "${html_file}"
+#echo "<div class='outer'>" >> "${html_file}"
+#echo "<div class='inner'>" >> "${html_file}"
+##echo "<h2 class='heading'>List of Processed Files</h2>" >> "${html_file}"
+#echo "<h2 class='heading'>🦾 Real AI Leaderboards 🦿</h2>" >> "${html_file}"
+#echo "<table class='file-list'>" >> "${html_file}"
+#for file_name in "${processed_files[@]}"; do
+#  link_name="${file_name}.html"
+#  echo "<tr><td><a href='${link_name}' class='file-link'>${file_name}</a></td></tr>" >> "${html_file}"
+#done
+#echo "</table>" >> "${html_file}"
+#echo "</div>" >> "${html_file}"
+#echo "</div>" >> "${html_file}"
+#echo "</body>" >> "${html_file}"
+#echo "</html>" >> "${html_file}"
+#echo "List of processed files written to: ${html_file}"
 
