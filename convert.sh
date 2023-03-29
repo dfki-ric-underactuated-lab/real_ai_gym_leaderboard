1ce48dbfcb82aea2b3eb73e55e6aa1ac0a7f61a9
Lasse <lasse.jenn.may@gmail.com>
2023-03-29 14:46:48 +0200

    removed dummy data and updated title of leaderboards

--- convert.sh
+++ convert.sh
@@ -13,7 +13,6 @@ for csv_file in ${csv_dir}/*.csv; do
   # get the file_name
   file_name=$(basename ${csv_file} .csv)
 
-
   to_md_cmd="pandoc -s -o $out_dir/$file_name.md -f csv --metadata title=$file_name $csv_file"
   # convert table to md
   echo "Running command: ${to_md_cmd}"
@@ -33,16 +32,18 @@ for csv_file in ${csv_dir}/*.csv; do
     cat $file_name.md >> $out_dir/$file_name.md
 
   fi
-  # convert to html
-  to_html_cmd="pandoc -s -o $out_dir/$file_name.html --mathjax --standalone --metadata title=$file_name --css=static/style.css --template=templates/default.html5 --include-after=static/style_tables.js $out_dir/$file_name.md"
- 
 
-  #replace_mathjax_src='src="/usr/share/javascript/mathjax/tex-mml-chtml.js"'
-  #replace_mathjax_src='src="/usr/share/javascript/mathjax/tex-mml-chtml.js"'
+
+  # convert to html but changing the title
+  heading_text_prefix="Welcome\ to\ the\ "
+  heading_text=$(echo "$file_name" | sed -e 's/_/\ /g' -e 's/\b\(.\)/\u\1/g' -e "s/^/${heading_text_prefix}/")
+  to_html_cmd="pandoc -s -o $out_dir/$file_name.html --mathjax --standalone --metadata title='"$heading_text"' \
+	  --css=static/style.css --template=templates/default.html5 --include-after=static/style_tables.js $out_dir/$file_name.md"
+ 
 
   # Run the command on the current CSV file
   echo "Running command: ${to_html_cmd}"
-  ${to_html_cmd}
+  echo ${to_html_cmd} | bash
 
   # add to list of files
   processed_files+=("$file_name")
@@ -60,38 +61,11 @@ placeholder="<p>:::leaderboard-links:::</p>"
 table="<table>"
 for file_name in "${processed_files[@]}"; do
   link_name="${file_name}.html"
-  table+="<tr><td><a href='${link_name}' class='file-link'>${file_name}</a></td></tr>"
+  table_text_prefix="> "
+  table_text=$(echo "$file_name" | sed -e 's/_/ /g' -e 's/\b\(.\)/\u\1/g' -e "s/^/${table_text_prefix} /")
+  table+="<tr><td><a href='${link_name}' class='file-link'>${table_text}</a></td></tr>"
 done
 table+="</table>"
 
 sed -i "s|${placeholder}|${table}|g" $out_dir/index.html
-
-# generate quick and dirty landing page
-#html_file="$out_dir/index.html"
-#echo "<html>" > "${html_file}"
-#echo "<head>" >> "${html_file}"
-#echo "<link rel='stylesheet' href='static/style.css'>" >> "${html_file}"
-#echo "<style>" >> "${html_file}"
-#echo ".outer { display: flex; justify-content: center; align-items: center; height: 100vh; }" >> "${html_file}"
-#echo ".heading { text-align: center; }" >> "${html_file}"
-#echo ".file-list { margin-left: auto; margin-right: auto; }" >> "${html_file}"
-#echo ".file-link { font-weight: bold; }" >> "${html_file}"
-#echo "</style>" >> "${html_file}"
-#echo "</head>" >> "${html_file}"
-#echo "<body>" >> "${html_file}"
-#echo "<div class='outer'>" >> "${html_file}"
-#echo "<div class='inner'>" >> "${html_file}"
-##echo "<h2 class='heading'>List of Processed Files</h2>" >> "${html_file}"
-#echo "<h2 class='heading'>🦾 Real AI Leaderboards 🦿</h2>" >> "${html_file}"
-#echo "<table class='file-list'>" >> "${html_file}"
-#for file_name in "${processed_files[@]}"; do
-#  link_name="${file_name}.html"
-#  echo "<tr><td><a href='${link_name}' class='file-link'>${file_name}</a></td></tr>" >> "${html_file}"
-#done
-#echo "</table>" >> "${html_file}"
-#echo "</div>" >> "${html_file}"
-#echo "</div>" >> "${html_file}"
-#echo "</body>" >> "${html_file}"
-#echo "</html>" >> "${html_file}"
-#echo "List of processed files written to: ${html_file}"
 
--- leaderboard0.md
+++
@@ -1,7 +0,0 @@
-## description of leaderboard 0
-you can put stuff here to describe the leaderboard.
-even formulas should work?
-
-$$
-1+1=2
-$$
